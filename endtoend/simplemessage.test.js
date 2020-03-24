085fe6aa2540af064c13739f06777628e179fd50
dvalentina <theviolintea@gmail.com>
2020-03-24 19:11:25 +0300

    проверка другого кода

--- endtoend/simplemessage.test.js
+++ endtoend/simplemessage.test.js
@@ -4,51 +4,51 @@ import 'regenerator-runtime/runtime';
 
 const path = require('path');
 
-describe('app', () => {
-	beforeAll(async () => {
-		await page.goto('http://localhost:3000/');
-	}, 60000);
+// describe('app', () => {
+// 	beforeAll(async () => {
+// 		await page.goto('http://localhost:3000/');
+// 	}, 60000);
 
-	it('should check the page', async () => {
-		await expect(page).toMatch('Messenger');
-	}, 10000);
+// 	it('should check the page', async () => {
+// 		await expect(page).toMatch('Messenger');
+// 	});
 
-	it('should open trash chat and return to the chatlist screen', async () => {
+// 	it('should open trash chat and return to the chatlist screen', async () => {
 
-	});
+// 	});
 
-	it('should create a new chat, open it and send a message', async () => {
+// 	it('should create a new chat, open it and send a message', async () => {
 
-	});
-});
+// 	});
+// });
 
 const puppeteer = require('puppeteer');
 
-// describe('chat', () => {
-// 	beforeAll(async () => {
-// 		const browser = await puppeteer.launch();
-// 		const page = await browser.newPage();
-// 		await page.goto('http://localhost:3000/');
-// 	});
+describe('chat', () => {
+	beforeAll(async () => {
+		const browser = await puppeteer.launch();
+		const page = await browser.newPage();
+		await page.goto('http://localhost:3000/');
+	});
 
-// 	it('chat must be created', async () => {
-// 		await expect(page).toClick('#CreateChat');
-// 	});
+	it('chat must be created', async () => {
+		await expect(page).toClick('#CreateChat');
+	});
 
-// 	it('chat must be open', async () => {
-// 		await expect(page).toClick('#chat_id_0');
-// 	});
+	it('chat must be open', async () => {
+		await expect(page).toClick('#chat_id_0');
+	});
 
-// 	it('message must be sent', async () => {
-// 		await expect(page).toFill('#message_input', 'Hello');
-// 		await page.keyboard.press('Enter');
-// 	});
+	it('message must be sent', async () => {
+		await expect(page).toFill('#message_input', 'Hello');
+		await page.keyboard.press('Enter');
+	});
 
-// 	it('message must be displayed', async () => {
-// 		await expect(page).toMatchElement('#message_text', {text: 'Hello'});
-// 	});
+	it('message must be displayed', async () => {
+		await expect(page).toMatchElement('#message_text', {text: 'Hello'});
+	});
 	
-// 	afterAll(async () => {
-// 		await browser.close();
-// 	});
-// });
+	afterAll(async () => {
+		await browser.close();
+	});
+});
