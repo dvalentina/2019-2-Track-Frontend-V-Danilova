c5703f27af08f9e571c628dd381d8366fc5b0e8e
dvalentina <theviolintea@gmail.com>
2020-03-24 19:14:49 +0300

    небольшие изменения

--- endtoend/simplemessage.test.js
+++ endtoend/simplemessage.test.js
@@ -4,51 +4,22 @@ import 'regenerator-runtime/runtime';
 
 const path = require('path');
 
-// describe('app', () => {
-// 	beforeAll(async () => {
-// 		await page.goto('http://localhost:3000/');
-// 	}, 60000);
-
-// 	it('should check the page', async () => {
-// 		await expect(page).toMatch('Messenger');
-// 	});
-
-// 	it('should open trash chat and return to the chatlist screen', async () => {
-
-// 	});
-
-// 	it('should create a new chat, open it and send a message', async () => {
-
-// 	});
-// });
-
-const puppeteer = require('puppeteer');
-
-describe('chat', () => {
+describe('app', () => {
 	beforeAll(async () => {
-		const browser = await puppeteer.launch();
-		const page = await browser.newPage();
 		await page.goto('http://localhost:3000/');
+	}, 60000);
+
+	it('should check the page', async () => {
+		await expect(page).toMatch('Messenger');
 	});
 
-	it('chat must be created', async () => {
-		await expect(page).toClick('#CreateChat');
+	it('should open trash chat and return to the chatlist screen', async () => {
+
 	});
 
-	it('chat must be open', async () => {
-		await expect(page).toClick('#chat_id_0');
-	});
+	it('should create a new chat, open it and send a message', async () => {
 
-	it('message must be sent', async () => {
-		await expect(page).toFill('#message_input', 'Hello');
-		await page.keyboard.press('Enter');
-	});
-
-	it('message must be displayed', async () => {
-		await expect(page).toMatchElement('#message_text', {text: 'Hello'});
-	});
-	
-	afterAll(async () => {
-		await browser.close();
 	});
 });
+
+
