2fd8018a456e35bf7f4da52cbd83c7d2efe53f3c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-14 16:31:54 +0000

    Deploy dvalentina/2019-2-Track-Frontend-V-Danilova to github.com/dvalentina/2019-2-Track-Frontend-V-Danilova.git:gh-pages

---
+++ precache-manifest.a868b79b453c466cc3cc4842eeeeda0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129ac94afd0e4d92b50778691b001aa4",
+    "url": "/2019-2-Track-Frontend-V-Danilova/index.html"
+  },
+  {
+    "revision": "1cfff6edf660ea2d5b95",
+    "url": "/2019-2-Track-Frontend-V-Danilova/static/css/main.ce604ddd.chunk.css"
+  },
+  {
+    "revision": "a6a62ed2f9bffd356d7a",
+    "url": "/2019-2-Track-Frontend-V-Danilova/static/js/2.48ede91a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/2019-2-Track-Frontend-V-Danilova/static/js/2.48ede91a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cfff6edf660ea2d5b95",
+    "url": "/2019-2-Track-Frontend-V-Danilova/static/js/main.dd520ab2.chunk.js"
+  },
+  {
+    "revision": "70a59ad31db35f428a61",
+    "url": "/2019-2-Track-Frontend-V-Danilova/static/js/runtime-main.4536a1b2.js"
+  },
+  {
+    "revision": "f330b7ad5e7c613bc3cdf2f51bee2cad",
+    "url": "/2019-2-Track-Frontend-V-Danilova/static/media/arrow.f330b7ad.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2019-2-Track-Frontend-V-Danilova/static/media/logo.5d5d9eef.svg"
+  }
+]);
